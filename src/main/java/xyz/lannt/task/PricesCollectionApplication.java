adad5d098c50afd555693d39e34b3d6a365dc43a
Nguyen Thanh Lan <mr.n.anrew@gmail.com>
2017-10-31 23:15:17 +0700

    change name

---
+++ src/main/java/xyz/lannt/task/PricesCollectionApplication.java
@@ -0,0 +1,46 @@
+package xyz.lannt.task;
+
+import static reactor.bus.selector.Selectors.$;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import reactor.Environment;
+import reactor.bus.EventBus;
+import xyz.lannt.task.application.Publisher;
+import xyz.lannt.task.application.Receiver;
+import xyz.lannt.task.application.property.DataCollectorProperty;
+
+@SpringBootApplication
+public class PricesCollectionApplication implements CommandLineRunner {
+
+  @Autowired
+  private DataCollectorProperty property;
+
+  @Autowired
+  private EventBus eventBus;
+
+  @Autowired
+  private Receiver receiver;
+
+  @Autowired
+  private Publisher publisher;
+
+  @Override
+  public void run(String... args) throws Exception {
+    eventBus.on($("quotes"), receiver);
+    publisher.publish(property.getSize());
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    ApplicationContext app = SpringApplication.run(PricesCollectionApplication.class, args);
+    app.getBean(CountDownLatch.class).await(5, TimeUnit.SECONDS);
+    app.getBean(Environment.class).shutdown();
+  }
+}
