edc86550c7120eb35784fcf2315a1eaabad933bb
anrew <mr.anrew@gmai.com>
2017-12-02 13:50:25 +0700

    add table

--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -1,5 +1,12 @@
+CREATE TABLE `develop`.`exchanges` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NULL,
+  `DisplayName` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`));
+
 CREATE TABLE `develop`.`marketsummaries` (
   `ID` INT NOT NULL AUTO_INCREMENT,
+  `ExchangeID` INT NOT NULL,
   `MarketName` VARCHAR(45) NULL,
   `Hight` FLOAT NULL,
   `Low` FLOAT NULL,
