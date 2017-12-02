2f7ce0ce88e21214472433be4a0b578da1aedce4
anrew <mr.anrew@gmai.com>
2017-12-02 16:38:56 +0700

    permission modification

--- script/startup-script.sh
+++ script/startup-script.sh
@@ -7,7 +7,9 @@ git clean -f
 git checkout prices-collection-implement
 git pull
 
-sudo ./gradlew clean build
+chmod +x gradlew
+
+./gradlew clean build
 
 export COMPOSE_FILE=./docker/docker-compose.yml
 docker-compose down
